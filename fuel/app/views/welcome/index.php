da1cde6335de1dfcee27730fdd4563b122233475
Dars <dars94@gmail.com>
2012-06-29 03:37:14 +0800

    first commit

---
+++ fuel/app/views/welcome/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>環警隊 - 財產管理</title>
+<meta charset="utf-8">
+<?php echo Asset::css('mos-style.css');?>
+</head>
+
+<body>
+<div id="header">
+	<div class="inHeaderLogin"></div>
+</div>
+
+<div id="loginForm">
+	<div class="headLoginForm">
+	管理者登入
+	</div>
+	<div class="fieldLogin">
+		<?php echo Form::open();?>
+		<?php echo Form::label('帳號：')?>
+		<?php echo Form::input('username','',array('class'=>'login'));?>
+		<?php echo Form::label('密碼：')?>
+		<?php echo Form::password('username','',array('class'=>'login'));?>
+		<?php echo Form::submit(array('value'=>'登入','class'=>'button'));?>
+		<?php echo Form::close();?>
+	</div>
+</div>
+</body>
+</html>
