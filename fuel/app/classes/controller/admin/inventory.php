3add13b42560ff4f532efb90c05cdde7bb30e1e4
Dars <dars94@gmail.com>
2012-11-21 02:42:57 +0800

    抑制錯誤畫面..

--- fuel/app/classes/controller/admin/inventory.php
+++ fuel/app/classes/controller/admin/inventory.php
@@ -106,6 +106,7 @@ class Controller_Admin_Inventory extends Controller_admin
 		if(Input::get('expiration_time')!=NULL || isset($condition['expiration_time'])){
 			$query = DB::query('UPDATE assets SET `expiration_time` = DATEDIFF(`expire_date` , `buy_date`)');
 			$query->execute();
+
 			$condition['expiration_time'] = Input::get('expiration_time', @$condition['expiration_time']);
 			$result->where('expiration_time',Input::get('expiration_time', @$condition['expiration_time']));
 		}
@@ -411,122 +412,126 @@ class Controller_Admin_Inventory extends Controller_admin
 	{
 		$this->template = false;
 		$chks = Session::get('chks');
-		$objs = Model_Assets::find()->where('id','IN',$chks)->get();
+		if(is_array($chks)){
+			$objs = Model_Assets::find()->where('id','IN',$chks)->get();
 
-		Autoloader::add_class('PHPExcel',APPPATH.'vendor/PHPExcel.php');
-		$title = '環 保 警 察 隊 財 產 清 冊';
-		$xls_obj = new PHPExcel();
-		//設定標題
-		$xls_obj->getProperties()->setCategory($title);
-		$xls_obj->setActiveSheetIndex(0);
-		$xls_obj->getActiveSheet()->setTitle('財產清冊');
-		$xls_obj->getActiveSheet()->mergeCells('A1:M1');
-		$xls_obj->getActiveSheet()->getRowDimension('1')->setRowHeight(26);
-		$xls_obj->getActiveSheet()->getStyle('A1')->getFont()->getColor()->setARGB(PHPExcel_Style_Color::COLOR_DARKGREEN);
-		$xls_obj->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
-		$xls_obj->getActiveSheet()->getStyle('A1')->getFont()->setSize(14);
-		$xls_obj->getActiveSheet()->getStyle('A1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-		$xls_obj->getActiveSheet()->getStyle('A1')->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-		$xls_obj->getActiveSheet()->setCellValue('A1', $title);
-
-		$xls_obj->getActiveSheet()->getRowDimension('2')->setRowHeight(18);
-		$xls_obj->getActiveSheet()->setCellValue('A2', '項次');
-		$xls_obj->getActiveSheet()->setCellValue('B2', '總號');
-		$xls_obj->getActiveSheet()->setCellValue('C2', '分類號碼');
-		$xls_obj->getActiveSheet()->setCellValue('D2', '品名');
-		$xls_obj->getActiveSheet()->setCellValue('E2', '數量');
-		$xls_obj->getActiveSheet()->setCellValue('F2', '購置日');
-		$xls_obj->getActiveSheet()->setCellValue('G2', '年限');
-		$xls_obj->getActiveSheet()->setCellValue('H2', '金額');
-		$xls_obj->getActiveSheet()->setCellValue('I2', '放置地點');
-		$xls_obj->getActiveSheet()->setCellValue('J2', '保管人');
-		$xls_obj->getActiveSheet()->setCellValue('K2', '到期日');
-		$xls_obj->getActiveSheet()->setCellValue('L2', '逾期時間');
-		$xls_obj->getActiveSheet()->setCellValue('M2', '備註');
-
-		$start_row = 3;
-		$index = 1;
-		foreach($objs as $t):
-			$xls_obj->getActiveSheet()->setCellValue('A'.$start_row, $index);
-			$xls_obj->getActiveSheet()->setCellValue('B'.$start_row, $t->total_no);
-			$xls_obj->getActiveSheet()->setCellValue('C'.$start_row, $t->sub_no);
-			$xls_obj->getActiveSheet()->setCellValue('D'.$start_row, $t->name);
-			$xls_obj->getActiveSheet()->setCellValue('E'.$start_row, $t->qty);
-			$xls_obj->getActiveSheet()->setCellValue('F'.$start_row, $t->buy_date);
-			$xls_obj->getActiveSheet()->setCellValue('G'.$start_row, $t->years);
-			$xls_obj->getActiveSheet()->setCellValue('H'.$start_row, number_format($t->amount,2));
-			$xls_obj->getActiveSheet()->setCellValue('I'.$start_row, $t->location->name);
-			$xls_obj->getActiveSheet()->setCellValue('J'.$start_row, $t->user->name);
-			$xls_obj->getActiveSheet()->setCellValue('K'.$start_row, $t->expire_date);
-			$xls_obj->getActiveSheet()->setCellValue('L'.$start_row, Model_Assets::get_expire_day($t->expire_date));
-			$xls_obj->getActiveSheet()->setCellValue('M'.$start_row, $t->note);
-			$index++;
-			$start_row++;
-		endforeach;
-
-		//設定格式到A2~M2欄位
-		$xls_obj->getActiveSheet()->getStyle('A2')->getFont()->getColor()->setARGB(PHPExcel_Style_Color::COLOR_BLUE);
-		$xls_obj->getActiveSheet()->getStyle('A2')->getFont()->setBold(true);
-		$xls_obj->getActiveSheet()->getStyle('A2')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-		$xls_obj->getActiveSheet()->getStyle('A2')->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-		$xls_obj->getActiveSheet()->getStyle('A2')->getFont()->setSize(11);
-		$xls_obj->getActiveSheet()->getStyle('A2')->getBorders()->getTop()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->getStyle('A2')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->getStyle('A2')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->getStyle('A2')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
-		$xls_obj->getActiveSheet()->getStyle('A2')->getFill()->getStartColor()->setARGB(PHPExcel_Style_Color::COLOR_YELLOW);
-		$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('A2'), 'B2:M2' );
-		$xls_obj->getActiveSheet()->getStyle('M2')->getBorders()->getRight()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->getColumnDimension('A')->setWidth(5);
-		$xls_obj->getActiveSheet()->getColumnDimension('B')->setWidth(10);
-		$xls_obj->getActiveSheet()->getColumnDimension('C')->setWidth(11);
-		$xls_obj->getActiveSheet()->getColumnDimension('D')->setWidth(15);
-		$xls_obj->getActiveSheet()->getColumnDimension('E')->setWidth(5);
-		$xls_obj->getActiveSheet()->getColumnDimension('F')->setWidth(10);
-		$xls_obj->getActiveSheet()->getColumnDimension('G')->setWidth(5);
-		$xls_obj->getActiveSheet()->getColumnDimension('H')->setWidth(12);
-		$xls_obj->getActiveSheet()->getColumnDimension('I')->setWidth(11);
-		$xls_obj->getActiveSheet()->getColumnDimension('J')->setWidth(8);
-		$xls_obj->getActiveSheet()->getColumnDimension('k')->setWidth(10);
-		$xls_obj->getActiveSheet()->getColumnDimension('L')->setWidth(10);
-		$xls_obj->getActiveSheet()->getColumnDimension('M')->setWidth(20);
-		$n = $index+2;
-
-		$xls_obj->getActiveSheet()->getStyle('A3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-		$xls_obj->getActiveSheet()->getStyle('A3')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->getStyle('A3')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('A3'), 'A4:A'.$n );
-		$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('A3'), 'E3:G'.$n );
-		$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('A3'), 'I3:M'.$n );
-		$xls_obj->getActiveSheet()->getStyle('L3')->getBorders()->getRight()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('L3'), 'L4:L'.$n );
-		$xls_obj->getActiveSheet()->getStyle('B3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
-		$xls_obj->getActiveSheet()->getStyle('B3')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->getStyle('B3')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('B3'), 'B3:D'.$n );
-		$xls_obj->getActiveSheet()->getStyle('D3')->getFont()->setSize(9);
-		$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('D3'), 'D4:D'.$n );
-		$xls_obj->getActiveSheet()->getStyle('H3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_RIGHT);
-		$xls_obj->getActiveSheet()->getStyle('H3')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->getStyle('H3')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('H3'), 'H4:H'.$n);
-		$xls_obj->getActiveSheet()->getStyle('M3')->getFont()->setSize(9);
-		$xls_obj->getActiveSheet()->getStyle('M3')->getBorders()->getRight()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
-		$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('M3'), 'M4:M'.$n );
-
-		$xls_obj->getActiveSheet()->getDefaultStyle()->getFont()->setName('Arial');
-		$xls_obj->getActiveSheet()->getPageSetup()->setRowsToRepeatAtTopByStartAndEnd(2, 2);
-		$xls_obj->getActiveSheet()->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
-		$xls_obj->getActiveSheet()->getPageSetup()->setPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4);
-
-		Autoloader::add_class('PHPExcel_Writer_Excel5',APPPATH.'vendor/PHPExcel/Writer/Excel5.php');
-		$objWriter = new PHPExcel_Writer_Excel5($xls_obj);
-		$file = " 環保警察隊財產清冊_".date('Ymd').".xls";
-		
-		header('Content-Type: application/vnd.ms-excel');
-		header('Content-Disposition: attachment; filename="'.$file.'"');
-		header('Cache-Control: max-age=0');
-		$objWriter = PHPExcel_IOFactory::createWriter($xls_obj, 'Excel5');
-		$objWriter->save('php://output');
+			Autoloader::add_class('PHPExcel',APPPATH.'vendor/PHPExcel.php');
+			$title = '環 保 警 察 隊 財 產 清 冊';
+			$xls_obj = new PHPExcel();
+			//設定標題
+			$xls_obj->getProperties()->setCategory($title);
+			$xls_obj->setActiveSheetIndex(0);
+			$xls_obj->getActiveSheet()->setTitle('財產清冊');
+			$xls_obj->getActiveSheet()->mergeCells('A1:M1');
+			$xls_obj->getActiveSheet()->getRowDimension('1')->setRowHeight(26);
+			$xls_obj->getActiveSheet()->getStyle('A1')->getFont()->getColor()->setARGB(PHPExcel_Style_Color::COLOR_DARKGREEN);
+			$xls_obj->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
+			$xls_obj->getActiveSheet()->getStyle('A1')->getFont()->setSize(14);
+			$xls_obj->getActiveSheet()->getStyle('A1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+			$xls_obj->getActiveSheet()->getStyle('A1')->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+			$xls_obj->getActiveSheet()->setCellValue('A1', $title);
+	
+			$xls_obj->getActiveSheet()->getRowDimension('2')->setRowHeight(18);
+			$xls_obj->getActiveSheet()->setCellValue('A2', '項次');
+			$xls_obj->getActiveSheet()->setCellValue('B2', '總號');
+			$xls_obj->getActiveSheet()->setCellValue('C2', '分類號碼');
+			$xls_obj->getActiveSheet()->setCellValue('D2', '品名');
+			$xls_obj->getActiveSheet()->setCellValue('E2', '數量');
+			$xls_obj->getActiveSheet()->setCellValue('F2', '購置日');
+			$xls_obj->getActiveSheet()->setCellValue('G2', '年限');
+			$xls_obj->getActiveSheet()->setCellValue('H2', '金額');
+			$xls_obj->getActiveSheet()->setCellValue('I2', '放置地點');
+			$xls_obj->getActiveSheet()->setCellValue('J2', '保管人');
+			$xls_obj->getActiveSheet()->setCellValue('K2', '到期日');
+			$xls_obj->getActiveSheet()->setCellValue('L2', '逾期時間');
+			$xls_obj->getActiveSheet()->setCellValue('M2', '備註');
+	
+			$start_row = 3;
+			$index = 1;
+			foreach($objs as $t):
+				$xls_obj->getActiveSheet()->setCellValue('A'.$start_row, $index);
+				$xls_obj->getActiveSheet()->setCellValue('B'.$start_row, $t->total_no);
+				$xls_obj->getActiveSheet()->setCellValue('C'.$start_row, $t->sub_no);
+				$xls_obj->getActiveSheet()->setCellValue('D'.$start_row, $t->name);
+				$xls_obj->getActiveSheet()->setCellValue('E'.$start_row, $t->qty);
+				$xls_obj->getActiveSheet()->setCellValue('F'.$start_row, $t->buy_date);
+				$xls_obj->getActiveSheet()->setCellValue('G'.$start_row, $t->years);
+				$xls_obj->getActiveSheet()->setCellValue('H'.$start_row, number_format($t->amount,2));
+				$xls_obj->getActiveSheet()->setCellValue('I'.$start_row, $t->location->name);
+				$xls_obj->getActiveSheet()->setCellValue('J'.$start_row, $t->user->name);
+				$xls_obj->getActiveSheet()->setCellValue('K'.$start_row, $t->expire_date);
+				$xls_obj->getActiveSheet()->setCellValue('L'.$start_row, Model_Assets::get_expire_day($t->expire_date));
+				$xls_obj->getActiveSheet()->setCellValue('M'.$start_row, $t->note);
+				$index++;
+				$start_row++;
+			endforeach;
+	
+			//設定格式到A2~M2欄位
+			$xls_obj->getActiveSheet()->getStyle('A2')->getFont()->getColor()->setARGB(PHPExcel_Style_Color::COLOR_BLUE);
+			$xls_obj->getActiveSheet()->getStyle('A2')->getFont()->setBold(true);
+			$xls_obj->getActiveSheet()->getStyle('A2')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+			$xls_obj->getActiveSheet()->getStyle('A2')->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+			$xls_obj->getActiveSheet()->getStyle('A2')->getFont()->setSize(11);
+			$xls_obj->getActiveSheet()->getStyle('A2')->getBorders()->getTop()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->getStyle('A2')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->getStyle('A2')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->getStyle('A2')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
+			$xls_obj->getActiveSheet()->getStyle('A2')->getFill()->getStartColor()->setARGB(PHPExcel_Style_Color::COLOR_YELLOW);
+			$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('A2'), 'B2:M2' );
+			$xls_obj->getActiveSheet()->getStyle('M2')->getBorders()->getRight()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->getColumnDimension('A')->setWidth(5);
+			$xls_obj->getActiveSheet()->getColumnDimension('B')->setWidth(10);
+			$xls_obj->getActiveSheet()->getColumnDimension('C')->setWidth(11);
+			$xls_obj->getActiveSheet()->getColumnDimension('D')->setWidth(15);
+			$xls_obj->getActiveSheet()->getColumnDimension('E')->setWidth(5);
+			$xls_obj->getActiveSheet()->getColumnDimension('F')->setWidth(10);
+			$xls_obj->getActiveSheet()->getColumnDimension('G')->setWidth(5);
+			$xls_obj->getActiveSheet()->getColumnDimension('H')->setWidth(12);
+			$xls_obj->getActiveSheet()->getColumnDimension('I')->setWidth(11);
+			$xls_obj->getActiveSheet()->getColumnDimension('J')->setWidth(8);
+			$xls_obj->getActiveSheet()->getColumnDimension('k')->setWidth(10);
+			$xls_obj->getActiveSheet()->getColumnDimension('L')->setWidth(10);
+			$xls_obj->getActiveSheet()->getColumnDimension('M')->setWidth(20);
+			$n = $index+2;
+	
+			$xls_obj->getActiveSheet()->getStyle('A3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+			$xls_obj->getActiveSheet()->getStyle('A3')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->getStyle('A3')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('A3'), 'A4:A'.$n );
+			$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('A3'), 'E3:G'.$n );
+			$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('A3'), 'I3:M'.$n );
+			$xls_obj->getActiveSheet()->getStyle('L3')->getBorders()->getRight()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('L3'), 'L4:L'.$n );
+			$xls_obj->getActiveSheet()->getStyle('B3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
+			$xls_obj->getActiveSheet()->getStyle('B3')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->getStyle('B3')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('B3'), 'B3:D'.$n );
+			$xls_obj->getActiveSheet()->getStyle('D3')->getFont()->setSize(9);
+			$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('D3'), 'D4:D'.$n );
+			$xls_obj->getActiveSheet()->getStyle('H3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_RIGHT);
+			$xls_obj->getActiveSheet()->getStyle('H3')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->getStyle('H3')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('H3'), 'H4:H'.$n);
+			$xls_obj->getActiveSheet()->getStyle('M3')->getFont()->setSize(9);
+			$xls_obj->getActiveSheet()->getStyle('M3')->getBorders()->getRight()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+			$xls_obj->getActiveSheet()->duplicateStyle( $xls_obj->getActiveSheet()->getStyle('M3'), 'M4:M'.$n );
+	
+			$xls_obj->getActiveSheet()->getDefaultStyle()->getFont()->setName('Arial');
+			$xls_obj->getActiveSheet()->getPageSetup()->setRowsToRepeatAtTopByStartAndEnd(2, 2);
+			$xls_obj->getActiveSheet()->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
+			$xls_obj->getActiveSheet()->getPageSetup()->setPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4);
+	
+			Autoloader::add_class('PHPExcel_Writer_Excel5',APPPATH.'vendor/PHPExcel/Writer/Excel5.php');
+			$objWriter = new PHPExcel_Writer_Excel5($xls_obj);
+			$file = " 環保警察隊財產清冊_".date('Ymd').".xls";
+			
+			header('Content-Type: application/vnd.ms-excel');
+			header('Content-Disposition: attachment; filename="'.$file.'"');
+			header('Cache-Control: max-age=0');
+			$objWriter = PHPExcel_IOFactory::createWriter($xls_obj, 'Excel5');
+			$objWriter->save('php://output');
+		}else{
+			echo '資料錯誤,請重新選取';
+		}
 	}
 }
